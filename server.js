45940dc4578d3b6381b0860832e474b4d7f8bb3d
cwi.lgomes1 <cwi.lgomes1@bv.com.br>
2022-06-20 23:00:00 -0300

    altera angular json nome app

--- server.js
+++ server.js
@@ -1,25 +1,28 @@
-function requireHTTPS(req, res, next) {
-  // The 'x-forwarded-proto' check is for Heroku
-  if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
-      return res.redirect('https://' + req.get('host') + req.url);
-  }
-  next();
-}
+// function requireHTTPS(req, res, next) {
+//   // The 'x-forwarded-proto' check is for Heroku
+//   if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
+//       return res.redirect('https://' + req.get('host') + req.url);
+//   }
+//   next();
+// }
 
 //Install express server
 const express = require('express');
 const path = require('path');
 
+const PORT = process.env.PORT || 8080;
+
 const app = express();
-app.use(requireHTTPS)
+// app.use(requireHTTPS)
 
 // Serve only the static files form the dist directory
 app.use(express.static('./dist/agenda-mecanica'));
 
 app.get('/*', function(req, res) {
-    res.sendFile('index.html', {root: 'dist./agenda-mecanica/'}
-);
+    res.sendFile(__dirname + './dist/agenda-mecanica/index.html');
 });
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+app.listen(PORT, () => {
+  console.log('servidor iniciado na porta: '+ PORT)
+});
