5cabe5cd0110af92c43e05c679be907cddad2aac
cwi.lgomes1 <cwi.lgomes1@bv.com.br>
2022-06-20 22:06:04 -0300

    altera dist server

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./agenda-mecanica'));
+app.use(express.static('.dist/agenda-mecanica'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: './agenda-mecanica'}),
+    res.sendFile('index.html', {root: 'dist./agenda-mecanica/index.html'}),
 );
 
 // Start the app by listening on the default Heroku port
